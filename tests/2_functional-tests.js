069cce629fad9ffd3fe8ce7bc4fed1d79b56bfcb
max-bondarenco <jaderystudion@gmail.com>
2023-05-03 22:36:30 +0300

    issue with tests passing on fcc

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,12 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
+const app = require("../app");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {});
 
+after(() => {
+  chai.request(app).get("/");
 });
-
